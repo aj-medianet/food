fbf5334aa8543e032e0f8ef25c1f1743b44dc723
Andrew Joseph <rcktmn411@gmail.com>
2021-11-07 12:39:34 -0700

    finished app

--- app.py
+++ app.py
@@ -6,14 +6,7 @@ from pathlib import Path
 
 st.sidebar.title('Food App')
 db_file = Path("./food.sqlite")
-
-# check sqlite DB file exists
-def connect_db():
-    if not db_file.is_file():
-        st.write('Database does not exist')
-        return False
-    else:
-        return sqlite3.connect(db_file)
+conn =sqlite3.connect(db_file)
 
 
 def show_all_restaurants(cur):
@@ -33,7 +26,8 @@ def get_restaurants(cur):
 
 
 def select_restaurant(restaurants):
-    return st.sidebar.selectbox('Select Restaurant', (restaurants))
+    return st.selectbox('Select Restaurant', (restaurants))
+
 
 def show_restaurant(cur, restaurant):
     query = "SELECT * FROM orders WHERE restaurant=\"%s\";" % (restaurant)
@@ -45,30 +39,72 @@ def show_restaurant(cur, restaurant):
     )
     st.table(df)
 
-def get_cols(cur):
-    cur.execute("SELECT * FROM orders")
-    return [description[0] for description in cur.description]
 
 def add_rest(cur, rest_name):
     query = "INSERT INTO restaurants VALUES(\'%s\');" % (rest_name)
     cur.execute(query)
+    conn.commit()
     st.success("Successfully added {}".format(rest_name))
 
-conn = connect_db()
+
+def del_rest(cur, rest_name):
+    query = "DELETE FROM restaurants WHERE name=\'%s\'" % (rest_name)
+    cur.execute(query)
+    conn.commit()
+    st.success("Successfully deleted {}".format(rest_name))
+
+
+def add_order(cur, rest_name, dish, rating):
+    st.write(rest_name, dish, rating)
+    name = ''.join(rest_name)
+    cur.execute("INSERT INTO orders(restaurant, dish, rating) VALUES(?, ?, ?)",(name, dish, rating))
+    conn.commit()
+    st.success("Successfully Added {}".format(dish))
+
+
 if conn:
     cur = conn.cursor()
-
-    with st.form(key="add_rest"):
-        st.write("Add New Restaurant")
-        rest_name = st.text_input("Restaurant Name")
-        submit_button = st.form_submit_button(label='Add')
-
-    if submit_button:
-        add_rest(cur, rest_name)
-
     restaurants = get_restaurants(cur)
-    restaurant = select_restaurant(restaurants)
+    
+    menu = ["Home", "Add Order", "Add Restaurant", "Delete Restaurant"]
+    choice = st.sidebar.selectbox("", menu)
 
-    if restaurant:
-        show_restaurant(cur, "".join(restaurant))
+    if choice == 'Home':
+        restaurant = select_restaurant(restaurants)
+        if restaurant:  
+            show_restaurant(cur, "".join(restaurant))
+    
+    elif choice == "Add Order":
+        # add rest form
+        with st.form(key="add_order"):
+            st.write("Add New Order")
+            rest_name = st.selectbox("Restaurant Name", restaurants)
+            dish = st.text_input("Dish")
+            rating = st.text_input("Rating")
+            submit_add_order = st.form_submit_button(label='Add Order')
+
+        if submit_add_order:
+            add_order(cur, rest_name, dish, rating)
+    
+    elif choice == "Add Restaurant":
+        # add rest form
+        with st.form(key="add_rest"):
+            st.write("Add New Restaurant")
+            add_rest_name = st.text_input("Restaurant Name")
+            submit_add = st.form_submit_button(label='Add')
+
+        if submit_add:
+            add_rest(cur, add_rest_name)
+        
+    elif choice == "Delete Restaurant": 
+        # delete rest form
+        with st.form(key='delete_rest'):
+            del_rest_name = st.selectbox('Select Restaurant to Delete', restaurants)
+            submit_delete = st.form_submit_button(label="Delete")
+
+        if submit_delete:
+            del_rest(cur, del_rest_name)
+
+
+    
 
